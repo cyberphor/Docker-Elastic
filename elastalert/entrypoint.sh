40403805b0fccc3e23277b4f37b6af3175127148
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2023-05-18 10:36:12 -0400

    Elasticsearch and ElastAlert are working!

--- elastalert/entrypoint.sh
+++ elastalert/entrypoint.sh
@@ -5,6 +5,8 @@ CERTS=(
     "/opt/elastalert/config/certs/elastalert.crt"
     "/opt/elastalert/config/certs/elastalert.key"
 )
+CUSTOM_INDEX="logstash"
+WRITEBACK_INDEX="elastalert_status"
 
 for CERT in "${CERTS[@]}"; do
     if [ ! -f $CERT ]; then
@@ -15,7 +17,25 @@ for CERT in "${CERTS[@]}"; do
     fi
 done
 
-# Create an Elasticsearch index and then run ElastAlert
-elastalert-create-index --config config/elastalert.yml &&\
-elastalert-create-index --config config/custom-index.yml &&\
+CUSTOM_INDEX_CHECK=$(curl --cacert config/certs/ca.crt -u elastic:elastic -s -o /dev/null -w "%{http_code}" -X GET -H "Content-Type: application/json" "https://172.16.0.10:9200/$CUSTOM_INDEX")
+if [ "$CUSTOM_INDEX_CHECK" == "200" ]; then
+  echo "Index '$CUSTOM_INDEX' already exists."
+else
+  CREATE_CUSTOM_INDEX=$(curl --cacert config/certs/ca.crt -u elastic:elastic -s -o /dev/null -w "%{http_code}" -X PUT -H "Content-Type: application/json" "https://172.16.0.10:9200/$CUSTOM_INDEX")
+  if [ "$CREATE_CUSTOM_INDEX" == "200" ]; then
+    echo "Created the '$CUSTOM_INDEX' index."
+  else
+    echo "Failed to create '$CUSTOM_INDEX' index."
+    exit 1
+  fi
+fi
+
+WRITEBACK_INDEX_CHECK=$(curl --cacert config/certs/ca.crt -u elastic:elastic -s -o /dev/null -w "%{http_code}" -X GET -H "Content-Type: application/json" "https://172.16.0.10:9200/$WRITEBACK_INDEX")
+if [ "$WRITEBACK_INDEX_CHECK" == "200" ]; then
+  echo "Index '$WRITEBACK_INDEX' already exists."
+else
+  elastalert-create-index --config config/elastalert.yml
+fi
+
+# Run ElastAlert
 python -m elastalert.elastalert --config config/elastalert.yml
