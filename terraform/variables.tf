fe931fcef19220517bfb93c6e48bee188743e452
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2024-02-01 11:05:20 -0500

    Debugging Docker Compose file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,33 @@ variable "location" {
   type = string
   default = "eastus"
 }
+
+variable "resource_group" {
+  type = string
+  default = null
+}
+
+variable "image" {
+  type    = string
+  default = null
+}
+
+variable "image_version" {
+  type    = string
+  default = "v1.0.0"
+}
+
+variable "container_port" {
+  type    = number
+  default = 1337
+}
+
+variable "share_name" {
+  type    = string
+  default = null
+}
+
+variable "volume_mount_path" {
+  type    = string
+  default = null
+}
